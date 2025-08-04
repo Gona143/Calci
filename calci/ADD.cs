707cb5ed7a4c5bf5412757f238796dea474ccbd8
Gona143 <sathishgona148@gmail.com>
2025-08-04 15:50:31 +0530

    Sathish commit

---
+++ calci/ADD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calci
+{
+    internal class ADD
+    {
+        public void add(ref int i, ref int j)
+        {
+            // int a = 20, b = 30;
+            /*Console.Write("Enter a value: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b value: ");
+           int j = Convert.ToInt32(Console.ReadLine());
+            */
+            int sum = i + j;
+            Console.Write("The final output is : ");
+            Console.WriteLine(sum.ToString());
+        }
+        
+    }
+}
---
+++ calci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a value: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b value: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+
+
+            ADD add1 = new ADD();
+            add1.add(ref i, ref j);
+
+            Console.ReadLine();
+        }
+    }
+}
